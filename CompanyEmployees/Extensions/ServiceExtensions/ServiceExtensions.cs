bd93ebd6643e9560038ab33c09d7d93b5da5eab9
minthihalwin <minthihalwin.dev@gmail.com>
2025-02-01 18:27:44 -0800

    feature: added draft 3 solutions(main solution and contracts and loggerService

---
+++ CompanyEmployees/Extensions/ServiceExtensions/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Extensions.ServiceExtensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+
+    }
+}
+
